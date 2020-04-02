6fb517e1c700c0fa18bfa4eb6c61609e46ef98cd
adam wojciechowski <adi61044@gmail.com>
2020-04-02 20:44:58 +0200

    working Imbalance class

--- Helper_functions.py
+++ Helper_functions.py
@@ -2,6 +2,9 @@ import numpy as np
 import abc 
 
 class Imbalance(abc.ABC):
+    #mu = |{minority classes}| / no_classes
+    #rho = max{|class_i|} / min{|class_i|}
+
     @staticmethod
     def get_set_distribution(labels):
         return dict(zip(*np.unique(labels, return_counts=True)))
@@ -9,8 +12,6 @@ class Imbalance(abc.ABC):
     @staticmethod
     def get_set_parameters(labels):
         #returns tuple(no_parameters, mu if step_imbalance detected else None, rho)
-        #mu = |{minority classes}| / no_classes
-        #rho = max{|class_i|} / min{|class_i|}
         #dictionary.keys() are no instances of a class
         #dictionary.values() are no classes with no of instances equal to its corresponding key
         array_of_numbers = np.unique(labels, return_counts = True)
@@ -21,11 +22,13 @@ class Imbalance(abc.ABC):
         rho = max(dictionary.keys()) / min(dictionary.keys())
         return (mu, rho)
 
-    @staticmethod
-    def change_set_statistics(images: np.ndarray, labels: np.ndarray, mu, rho):
+def change_set_statistics(images: np.ndarray, labels: np.ndarray, mu, rho):
     #check tensor dimensions
     if images.shape[0] != labels.shape[0]:
-        raise IndexError
+        raise IndexError('tensor dimensions are not matching')
+    #check parameter values
+    if mu <= 0 or rho <= 0:
+        raise ValueError('mu and rho are posive real numbers')
     array_dict = dict()
     #set majority class size to the minimal avaliable class size
     majority_size = np.min(np.unique(labels, return_counts=True)[1])
@@ -48,7 +51,8 @@ class Imbalance(abc.ABC):
         #init output np arrays
         out_labels = np.empty(shape = tuple(labels.shape[i] if i != 0
                                             else 0
-                                            for i in range(len(labels.shape))))
+                                            for i in range(len(labels.shape))), 
+                              dtype = np.int32)
         out_images = np.empty(shape = tuple(images.shape[i] if i != 0 
                                             else 0
                                             for i in range(len(images.shape))))
@@ -58,16 +62,46 @@ class Imbalance(abc.ABC):
             tmp_labels = np.full(shape = tuple(labels.shape[i] if i != 0
                                             else minority_size
                                             for i in range(len(labels.shape))), 
-                                 fill_value = list_of_labels[i])
+                                 fill_value = list_of_labels[i], 
+                                 dtype = np.int32)
             out_images = np.concatenate((out_images, array_dict[list_of_labels[i]]))
             out_labels = np.concatenate((out_labels, tmp_labels))
         for i in range(minority_no, len(array_dict), 1):
             tmp_labels = np.full(shape = tuple(labels.shape[i] if i != 0
                                             else majority_size
                                             for i in range(len(labels.shape))), 
-                                 fill_value = list_of_labels[i])
+                                 fill_value = list_of_labels[i], 
+                                 dtype = np.int32)
             out_images = np.concatenate((out_images, array_dict[list_of_labels[i]]))
             out_labels = np.concatenate((out_labels, tmp_labels))
-        return (out_images, out_labels)
-    #todo: comment i jeszcze poshufflowac te out arrays np.permutations
-    #todo: mu is None    
+    #if linear imabalnce variant chosen
+    else:
+        #fromalize namespace
+        biggest_size = majority_size
+        smallest_size = minority_size
+        #create list of unique shuffled labels
+        list_of_labels = np.unique(labels)
+        np.random.shuffle(list_of_labels)
+        #init output np arrays
+        out_labels = np.empty(shape = tuple(labels.shape[i] if i != 0
+                                            else 0
+                                            for i in range(len(labels.shape))), 
+                              dtype = np.int32)
+        out_images = np.empty(shape = tuple(images.shape[i] if i != 0 
+                                            else 0
+                                            for i in range(len(images.shape))))
+        class_sizes = list(np.linspace(smallest_size, biggest_size, len(array_dict), dtype = np.int32))
+        for (i, class_size) in zip(range(len(array_dict)), class_sizes):
+            array_dict[list_of_labels[i]] = array_dict[list_of_labels[i]][:class_size]
+            tmp_labels = np.full(shape = tuple(labels.shape[i] if i != 0
+                                            else class_size
+                                            for i in range(len(labels.shape))), 
+                                 fill_value = list_of_labels[i], 
+                                 dtype = np.int32)
+            out_images = np.concatenate((out_images, array_dict[list_of_labels[i]]))
+            out_labels = np.concatenate((out_labels, tmp_labels))
+    #shuffle both of the final tensors in coequal manner
+    permutation = np.random.permutation(np.arange(out_images.shape[0]))
+    out_images = out_images[permutation]
+    out_labels = out_labels[permutation]
+    return (out_images, out_labels)
